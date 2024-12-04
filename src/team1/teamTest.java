e998cf94857174dac83b428cba4be19788b6bf25
byunicom <byunicom@gmail.com>
2024-12-04 22:04:43 +0900

    welcome message

--- src/team1/teamTest.java
+++ src/team1/teamTest.java
@@ -4,5 +4,7 @@ public class teamTest {
     public static void main(String[] args) {
         System.out.println("병우씨 반가워요...");
         System.out.println("드디어 팀 프로젝트 하는 법을 알았어요..");
+        System.out.println("-------------------2024.12.04 실패");
+        System.out.println("내일은 성공할 수 있겠지...");
     }
 }

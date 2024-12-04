6eca321e0cea258f7466c18eb0b1ca23e5079826
byunicom <byunicom@gmail.com>
2024-12-04 21:27:22 +0900

    first commit

---
+++ src/team1/teamTest.java
@@ -0,0 +1,8 @@
+package team1;
+
+public class teamTest {
+    public static void main(String[] args) {
+        System.out.println("병우씨 반가워요...");
+        System.out.println("드디어 팀 프로젝트 하는 법을 알았어요..");
+    }
+}

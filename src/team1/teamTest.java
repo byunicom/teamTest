3f0a469ba7c5d69f2951ef19c7f4062a1a3ad478
byunicom <byunicom@gmail.com>
2024-12-05 16:52:20 +0900

    ok

--- src/team1/teamTest.java
+++ src/team1/teamTest.java
@@ -6,5 +6,6 @@ public class teamTest {
         System.out.println("드디어 팀 프로젝트 하는 법을 알았어요..");
         System.out.println("-------------------2024.12.04 실패");
         System.out.println("내일은 성공할 수 있겠지...ggg");
+        System.out.println("병우씨 드디어 해냈어요.");
     }
 }

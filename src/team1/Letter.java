ba63f11479b9df719aab806459457ec8dbc313fe
byunicom <byunicom@gmail.com>
2024-12-07 00:02:46 +0900

    폴더 사라짐 해결 후 커밋

--- src/team1/Letter.java
+++ src/team1/Letter.java
@@ -1,6 +1,6 @@
 package team1;
 
-public class teamTest {
+public class Letter {
     public static void main(String[] args) {
         System.out.println("병우씨 반가워요...");
         System.out.println("드디어 팀 프로젝트 하는 법을 알았어요..");

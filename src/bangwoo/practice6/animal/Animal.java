670dd13a237a09fe25eccb3ba2b2a3f6693cddc0
unknown <quddn2917@gmail.com>
2024-12-10 21:33:15 +0900

    ãpractice6/상속 추가

---
+++ src/bangwoo/practice6/animal/Animal.java
@@ -0,0 +1,14 @@
+package bangwoo.practice6.animal;
+
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+        System.out.println("동물이 소리를 냅니다.");
+    }
+}
+

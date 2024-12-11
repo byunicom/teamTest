d252b46b8cf17204073215dc011976e67931c5ad
byunicom <byunicom@gmail.com>
2024-12-12 08:13:07 +0900

    20241211_Practice upload 2

--- src/dgkim/P1211/Character.java
+++ src/dgkim/P1211/Character.java
@@ -5,7 +5,6 @@ public class Character {
 
     public void setAttackStrategy(Attackable strategy){
         this.attackStrategy = strategy;
-        System.out.println(this.attackStrategy);
     }
 
     public void performAttack(){
@@ -15,9 +14,12 @@ public class Character {
     public static void main(String[] args) {
         Character character = new Character();
 
-        //character.setAttackStrategy(character.attackStrategy);
-        //character.performAttack();
 
+        character.setAttackStrategy(new SwordAttack());
+        character.performAttack();
+
+        character.setAttackStrategy(new BowAttack());
+        character.performAttack();
 
     }
 }
